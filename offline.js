3074b9d7d353bab9a878caa6a43094797b60c06e
Vinay Verma <56758330+vermavinay8948@users.noreply.github.com>
2021-10-02 22:52:28 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1569700324,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/player-sheet0.png",
+		"images/spider-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/explosion-sheet0.png",
+		"images/gun-sheet0.png",
+		"images/gun2-sheet0.png",
+		"images/scorpian-sheet0.png",
+		"images/scorpian2-sheet0.png",
+		"media/throwknife2.m4a",
+		"media/throwknife2.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
